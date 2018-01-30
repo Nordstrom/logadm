c32881409803d41b24e1d2ff6ba297bdf8556912
aexd <harman.dhunna@nordstrom.com>
2018-01-29 19:07:19 -0800

    - License change. -

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,42 +1,25 @@
-# encoding: utf-8
-# Copyright (c) 2013 ModCloth, Inc.
+# Copyright (C) 2018 Nordstrom Inc.
 #
-# MIT License
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of
-# this software and associated documentation files (the "Software"), to deal in
-# the Software without restriction, including without limitation the rights to
-# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-# of the Software, and to permit persons to whom the Software is furnished to do
-# so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# All rights reserved - Do Not Redistribute
 #
 
-default['logadm']['write_conf_file'] = true
-default['logadm']['conf_file'] = '/etc/logadm.conf'
-default['logadm']['conf_file_template'] = 'logadm.conf.erb'
+cookbook_name = 'logadm'
 
-default['logadm']['enabled_patterns']['smf'] = true
-default['logadm']['enabled_patterns']['apache'] = false
-default['logadm']['enabled_patterns']['lighttpd'] = false
-default['logadm']['enabled_patterns']['nginx'] = false
-default['logadm']['enabled_patterns']['mysql'] = false
+default[cookbook_name]['write_conf_file'] = true
+default[cookbook_name]['conf_file'] = '/etc/logadm.conf'
+default[cookbook_name]['conf_file_template'] = 'logadm.conf.erb'
 
-default['logadm']['patterns']['smf'] = '-C 3 -c -s 1m /var/svc/log/*.log'
-default['logadm']['patterns']['apache'] = '-C 5 -c -s 100m /var/log/httpd/*.log'
-default['logadm']['patterns']['lighttpd'] =
+default[cookbook_name]['enabled_patterns']['smf'] = true
+default[cookbook_name]['enabled_patterns']['apache'] = false
+default[cookbook_name]['enabled_patterns']['lighttpd'] = false
+default[cookbook_name]['enabled_patterns']['nginx'] = false
+default[cookbook_name]['enabled_patterns']['mysql'] = false
+
+default[cookbook_name]['patterns']['smf'] = '-C 3 -c -s 1m /var/svc/log/*.log'
+default[cookbook_name]['patterns']['apache'] = '-C 5 -c -s 100m /var/log/httpd/*.log'
+default[cookbook_name]['patterns']['lighttpd'] =
   "-C 5 -c -s 100m '/var/log/lighttpd/{access,error}.log'"
-default['logadm']['patterns']['nginx'] =
+default[cookbook_name]['patterns']['nginx'] =
   "-C 5 -c -s 100m '/var/log/nginx/{access,error}.log'"
-default['logadm']['patterns']['mysql'] =
+default[cookbook_name]['patterns']['mysql'] =
   "-C 5 -c -s 100m '/var/log/mysql/{error,slowquery}.log'"
--- test/integration/default/inspec/default_test.rb
+++ test/integration/default/inspec/default_test.rb
@@ -3,5 +3,5 @@ logadm_conf_file = '/etc/logadm.conf'
 describe file(logadm_conf_file) do
   it { should exist }
   it { should be_file }
-  its('mode') { should cmp mode '00644' }
+  its('mode') { should cmp '0644' }
 end
